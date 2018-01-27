f2d5890f23ccb741641dd3e2d69e37f36958370a
Shahrukh Mustakim <shahrukh@umd.edu>
2018-01-27 08:54:42 -0500

    Temporary movement controls

--- js/bee.js
+++ js/bee.js
@@ -6,11 +6,48 @@ Bee = function(game){
     this.scale.setTo(0.5, 0.5);
     this.animations.add('idle', [0,1,2,3,4,5,6,7,8], this.speed, true);
     this.animations.play('idle');
+    this.state = "";
+    this.moveConst = 1;
+    
+    //temporary movement controls
+    game.input.keyboard.addKey(Phaser.KeyCode.LEFT).onDown.add(this.moveWest, this);
+    game.input.keyboard.addKey(Phaser.KeyCode.RIGHT).onDown.add(this.moveEast, this);
+    game.input.keyboard.addKey(Phaser.KeyCode.UP).onDown.add(this.moveNorth, this);
+    game.input.keyboard.addKey(Phaser.KeyCode.DOWN).onDown.add(this.moveSouth, this);
+    game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR).onDown.add(this.stopMoving, this);
 };
 
 Bee.prototype = Object.create(Phaser.Sprite.prototype);
 Bee.prototype.constructor = Bee;
+Bee.prototype.moveWest = function(){
+    this.state = "W";
+};
+Bee.prototype.moveEast = function(){
+    this.state = "E";
+};
+Bee.prototype.moveNorth = function(){
+    this.state = "N";
+};
+Bee.prototype.moveSouth = function(){
+    this.state = "S";
+};
+Bee.prototype.stopMoving = function(){
+    this.state = "";
+};
 Bee.prototype.update = function(){
 	Phaser.Sprite.prototype.update.call(this);
-    this.animations.play('idle');
+	switch(this.state) {
+        case "W":
+            this.x -= this.moveConst;
+            break;
+        case "E":
+            this.x += this.moveConst;
+            break;
+        case "N":
+            this.y -= this.moveConst;
+            break;
+        case "S":
+            this.y += this.moveConst;
+            break;
+    }
 };
