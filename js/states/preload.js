a2d558a247e59a5cd391e9c63299da1d0e8a308c
Ben Dodge <benjamin9dodge@gmail.com>
2018-01-26 23:04:55 -0500

    Fixed bug.

--- js/states/preload.js
+++ js/states/preload.js
@@ -4,6 +4,8 @@ var Preload = function(game){
 Preload.prototype = {
 	/* State methods */
 	init: function(params){
+        // prevent the space key from bubbling up to the web browser
+        game.input.keyboard.addKeyCapture(Phaser.KeyCode.SPACEBAR);
 	},
 	preload: function(){
 		// Use the script loader to avoid bloating the index with includes
